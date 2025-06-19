6e2e6875e34be08a6df87178c20e9ff4cc035c9a
Amirfarzam <amir-farzam.kamalpour@jvm.com>
2025-06-19 14:25:48 +0200

    Bug has been fixed

--- posts/views.py
+++ posts/views.py
@@ -4,21 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 
-@api_view(['GET'])
-def AllIndex(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def index(request, post_id):
-    try:
-        posts = Post.objects.get(id=post_id)
-        serializer = PostSerializer(posts)
-        return Response(serializer.data)
-    except Exception as e:
-        return Response({'detail':'Post not exist'}, status=400)
-
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'posts/post_list.html', {'posts': posts})
--- weblog/urls.py
+++ weblog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import post_list, index, post_detail
+from posts.views import post_list, post_detail
 from posts.api.api import AllIndex, index
 
 urlpatterns = [
